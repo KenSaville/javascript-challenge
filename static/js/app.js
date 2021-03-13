9fdef3d8bb77675a6505a39d54369db2de9f7363
KenSaville <45977942+KenSaville@users.noreply.github.com>
2021-03-13 15:41:37 -0500

    added data to table
    used D3 method

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// add data to table
+
+data.forEach((tableData) => {
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+
+// get button to work
+// example from class:   var upvote = d3.select(".upvote");
+
+var filterButton = d3.select(".btn-default");
+
+filterButton.on("click", function() {
+   console.log("you clicked it")
+  });
